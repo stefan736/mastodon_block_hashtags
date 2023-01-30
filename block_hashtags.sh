39e7a2dc02467e58a72a4d1b13865e646b4131a2
Stefan Berger <stefan@stefanberger.net>
2023-01-30 18:41:35 +0100

    added script

---
+++ block_hashtags.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+serverURL="https://social.yourdomain.net"
+applicationUserToken=$(cat API_USER_TOKEN.secret)
+
+myAccountId=$(curl -s -H "Authorization: Bearer $applicationUserToken" $serverURL/api/v1/accounts/verify_credentials | jq -r .id)
+
+get_all_followings () {
+    # get my followers to exclude from blocking
+    echo "" > followings.tmp
+    nextURL=$serverURL/api/v1/accounts/$myAccountId/following?limit=80
+    while :
+    do
+        curl -s -H "Authorization: Bearer $applicationUserToken" $nextURL | jq .[].id >> followings.tmp
+        curl -Is -w '%header{link}' -H "Authorization: Bearer $applicationUserToken" $nextURL > header.tmp
+        if ! grep -q 'rel="next"' "header.tmp"; then
+            break;
+        fi
+
+        nextURL=$(grep "link:" header.tmp | cut -d ' ' -f 2 | cut -d '>' -f 1 | cut -d '<' -f 2)
+    done
+}
+
+block_unknown_accounts_with_posts_tagged_with () {
+    tag=$1
+
+    while :
+    do
+        # get all accounts from posts with media for a given tag
+        accountIdsToBlock=$(curl -sf $serverURL/api/v1/timelines/tag/$tag | jq '.[] | select(.media_attachments | length > 0)' | jq -r .account.id | sort | uniq)
+        count=0
+        for accountId in $accountIdsToBlock; do
+            if [ "$accountId" != "$myAccountId" ]; then 
+                echo "$tag: checking account $accountId ..."
+                if ! grep -q "$accountId" "followings.tmp"; then
+                    echo "$tag: suspending account $accountId ..."
+                    curl -s -H "Authorization: Bearer $applicationUserToken" -X POST -F type=suspend $serverURL/api/v1/admin/accounts/$accountId/action
+                    echo "$tag: deleting account data $accountId ..."
+                    curl -s -H "Authorization: Bearer $applicationUserToken" -X DELETE -F type=suspend $serverURL/api/v1/admin/accounts/$accountId
+                fi
+                count=$[$count + 1]
+            fi
+        done
+
+        if [ $count -eq 0 ]; then 
+            break;
+        fi
+    done
+}
+
+echo
+echo "################### START $(date) #############"
+
+get_all_followings
+block_unknown_accounts_with_posts_tagged_with nude
+block_unknown_accounts_with_posts_tagged_with nsfw
+block_unknown_accounts_with_posts_tagged_with cock
+block_unknown_accounts_with_posts_tagged_with sex
+
+rm followings.tmp
+rm header.tmp
+
+echo "################### END $(date) #############"
