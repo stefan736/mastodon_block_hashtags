89cea792357144b1310fbac1988e40f9f142828f
nanos <nanos@users.noreply.github.com>
2023-02-07 13:36:27 +0000

    List tags in alphabetical order

--- config.sh
+++ config.sh
@@ -1,2 +1,18 @@
 serverURL='https://mstdn.thms.uk'
-tags=('nsfw' 'sex' 'cock' 'nude' 'nudemale' 'nudeman' 'gaynude' 'nakedguy' 'nudeart' 'boudoir' 'nudes' 'nudephotography' 'nudewoman' 'nudes' 'nudeboy' 'nudeoutdoors')
+tags=(
+    'boudoir'
+    'cock'
+    'gaynude'
+    'nakedguy'
+    'nsfw'
+    'nude'
+    'nudeart'
+    'nudeboy'
+    'nudemale'
+    'nudeman'
+    'nudeoutdoors'
+    'nudephotography'    
+    'nudes'
+    'nudewoman'
+    'sex'
+)
