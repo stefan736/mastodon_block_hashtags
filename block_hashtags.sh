0cec4c27e1ed85033d89bdea50327bbbe8ca87a5
Michael Thomas <nanos@thms.uk>
2023-02-08 08:09:58 +0000

    Paginate tags

--- block_hashtags.sh
+++ block_hashtags.sh
@@ -25,11 +25,14 @@ get_all_followings () {
 block_unknown_accounts_with_posts_tagged_with () {
     tag=$1
 
+    tagURL="$serverURL/api/v1/timelines/tag/$tag"
+
     while :
     do
+        echo "Getting $tagURL"
         # get all accounts from posts with media for a given tag
-        accountIdsToBlock=$(curl -sf $serverURL/api/v1/timelines/tag/$tag | jq '.[] | select(.media_attachments | length > 0)' | jq -r .account.id | sort | uniq)
-        count=0
+        accountIdsToBlock=$(curl -sf $tagURL | jq '.[] | select(.media_attachments | length > 0)' | jq -r .account.id | sort | uniq)
+        curl -Is -w '%header{link}' -H "Authorization: Bearer $accessToken" $tagURL > header.tmp
         for accountId in $accountIdsToBlock; do
             if [ "$accountId" != "$myAccountId" ]; then 
                 echo "$tag: checking account $accountId ..."
@@ -39,13 +42,14 @@ block_unknown_accounts_with_posts_tagged_with () {
                     echo "$tag: deleting account data $accountId ..."
                     curl -s -H "Authorization: Bearer $accessToken" -X DELETE -F type=suspend $serverURL/api/v1/admin/accounts/$accountId
                 fi
-                count=$[$count + 1]
             fi
         done
 
-        if [ $count -eq 0 ]; then 
+        if ! grep -q 'rel="next"' "header.tmp"; then
             break;
         fi
+
+        tagURL=$(grep "link:" header.tmp | cut -d ' ' -f 2 | cut -d '>' -f 1 | cut -d '<' -f 2)
     done
 }
 
